18fd24ccd3273d9b327883ebcedc1985b94f2416
Mrinmoy Mondal <mrinmoy002@gmail.com>
2020-08-26 00:54:41 -0400

    update

--- src/battery.cpp
+++ src/battery.cpp
@@ -7,6 +7,11 @@ enum batteryState{
     sink,
     off
 };
+extern objStoreStruct objStore;
+double max_current = .300;
+double max_voltage = 16.8;
+double user_max_voltage = 16.8; 
+
 battery::battery()
 {
 
@@ -37,5 +42,12 @@ void battery::runState()
 }
 
 int battery::chargeLevel(){
-    
+    //check to see 
+    if(batteryVoltage[4] == user_max_voltage)
+        return 0;
+    if(batteryVoltage[4] < user_max_voltage && objStore.charger.readCurrent(objStore.charger.bat_cur_pin) < max_current)
+        return 1;
+     
+     return -1;
+
 }
